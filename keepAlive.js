214918cb65c40d1e86edf1b73beb11425cf90ba4
omar-choura <omarchourao@gmail.com>
2025-05-15 20:06:14 +0200

    yup

--- keepAlive.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Bot is alive!'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-
